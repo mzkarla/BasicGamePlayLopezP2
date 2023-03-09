80dec74490b5b410c11097ddefb9f669692ccd89
mzkarla <714875@student.dusd.net>
2023-03-09 09:58:43 -0800

    finish
    finish

--- BasicGamePlayLopezP2/Assets/Scripts/PlayerController.cs
+++ BasicGamePlayLopezP2/Assets/Scripts/PlayerController.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float xRange = 10;
-
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 10.0f;
     public GameObject projectilePrefab;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -19,6 +27,10 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             //Launch a projectile from the player
